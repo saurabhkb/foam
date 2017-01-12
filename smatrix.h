649223c34ba2c35a669725967f77b2d2b0c9bfc8
saurabh <me>
2017-01-11 16:52:21 -0800

    cleaned up comments

--- smatrix.h
+++ smatrix.h
@@ -9,7 +9,7 @@ class SMatrix {
 private:
 	int screen_cursor_row;
 	int screen_cursor_col;
-	int y_offset;	/* line number from top of screen */
+	int y_offset;	/* offset from the top of the text matrix */
 public:
 	SMatrix();
 	~SMatrix();
