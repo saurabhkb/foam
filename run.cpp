7f69de1d81c90fe641b22f88fb2422cab85c257f
saurabh <me>
2017-01-11 16:28:18 -0800

    updated logic regarding screen window to show from text matrix

--- run.cpp
+++ run.cpp
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <string.h>
 #include "tmatrix.h"
 #include "smatrix.h"
 
-const int KEY_NEWLINE = 13;
-
-#define CEIL(a, b) (((a) / (b)) + (((a) % (b)) > 0 ? 1 : 0))
+inline unsigned int min(unsigned int a, unsigned int b) {
+	if(a > b) return b;
+	return a;
+}
 
 /* render the line with the string arr */
 int render_line(int pos, std::string arr) {
@@ -26,51 +26,39 @@ int render_line(int pos, std::string arr) {
 /* main function which goes through the text matrix and decides what submatrix actually
 appears on the screen based on the screen dimensions */
 void render(SMatrix& smtx, TMatrix& tmtx) {
-	unsigned int i = 0, len;
-	unsigned int text_line_number = 0, num_screen_lines = 0;
+	unsigned int i = 0;
 	clear();
 
-	for(unsigned int j = 0; j < tmtx.text.size(); j++) {
-		if(text_line_number == tmtx.logical_cursor_row)
-			smtx.screen_cursor_row = num_screen_lines + tmtx.logical_cursor_col / COLS;
-
-		/*
-		if line is empty, it will take up only one screen line otherwise,
-		the number of screen lines taken will depend on the length of the text line
-		and the number of columns in the screen
-		*/
-		len = tmtx.text[j].length();
-		num_screen_lines += (len == 0) ? 1 : CEIL(len, COLS);
-		text_line_number++;
-	}
+	/* shift screen window up or down if needed (if the size of the text matrix is bigger 
+	than the size of the screen window */
 	smtx.screen_cursor_col = tmtx.logical_cursor_col % COLS;
-
-	/*
-	increment the y_offset by the number of screen lines of the topmost line currently on the screen
-	note that we must increment i by the number of screen lines each text line occupies (not simply by 1 each time)
-	set y_offset as required */
-	if((int) (smtx.screen_cursor_row - smtx.y_offset) >= LINES) {
-		smtx.y_offset += num_screen_lines;
-	}
-	if(smtx.screen_cursor_row < smtx.y_offset) {
-		smtx.y_offset -= num_screen_lines;
-		if(smtx.y_offset < 0) smtx.y_offset = 0;
+	if(tmtx.logical_cursor_row - smtx.y_offset >= LINES) {
+		/* logical cursor is too far down, so update y offset */
+		smtx.y_offset = tmtx.logical_cursor_row - LINES + 1;
+		smtx.screen_cursor_row = tmtx.logical_cursor_row - smtx.y_offset;
+	} else if(tmtx.logical_cursor_row <= smtx.y_offset) {
+		/* logical cursor is above y offset */
+		smtx.y_offset = tmtx.logical_cursor_row;
+		smtx.screen_cursor_row = 0;
+	} else {
+		/* all normal */
+		smtx.screen_cursor_row = tmtx.logical_cursor_row - smtx.y_offset;
 	}
 
 	/* start rendering */
-	i = 0;
-	for(unsigned int j = smtx.y_offset; j < tmtx.text.size(); j++)
+	for(unsigned int j = smtx.y_offset; j < min((unsigned int) smtx.y_offset + LINES, tmtx.text.size()); j++) {
 		i += render_line(i, tmtx.text[j]);
+	}
 
-	/* move the cursor to the position calculated offset by the y_offset */
-	move(smtx.screen_cursor_row - smtx.y_offset, smtx.screen_cursor_col);
-	printw("nsl:%d, %d, %d\n", num_screen_lines, smtx.screen_cursor_row, LINES);
+	/* move the cursor to the position calculated */
+	move(smtx.screen_cursor_row, smtx.screen_cursor_col);
 	refresh();
 }
 
 /* loop which reads user input and calls render to show it on the screen */
 void run_loop(SMatrix& smtx, TMatrix& tmtx) {
 	unsigned int ch;
+	const int KEY_NEWLINE = 13;
 
 	render(smtx, tmtx);
 	while(1) {
@@ -109,7 +97,8 @@ int main() {
 	SMatrix smtx;
 	TMatrix tmtx;
 
-	initscr();	/* initialize */
+	initscr();	/* initialize; stick to the standard default window */
+	LINES = 10;
 	keypad(stdscr, TRUE);	/* extra keys */
 	cbreak();	/* no input buffering */
 	nonl();	/* better newline handling */
